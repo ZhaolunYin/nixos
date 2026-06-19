c5974dee9ed4c6908c539f52e47f4959594a0827
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-19 20:10:14 +0100

    changed stuff

--- rpi/modules/nixos/tailscale.nix
+++ rpi/modules/nixos/tailscale.nix
@@ -1,16 +1,13 @@
-{ pkgs, ... }:
-
 {
-	# Enable Tailscale with subnet routing
-	services.tailscale = {
-		enable = true;
-		# Enable subnet routing support
-		useRoutingFeatures = "server";
-		# Routes to advertise — change to match your local network
-		extraUpFlags = [ "--advertise-routes=192.168.1.0/24" ];
-	};
-
-	# Open necessary ports for Tailscale
-	networking.firewall.allowedUDPPorts = [ 41641 ];
-	networking.firewall.allowedTCPPorts = [ 443 ];
+    # Enable Tailscale with subnet routing
+    services.tailscale = {
+        enable = true;
+        useRoutingFeatures = "server";
+        extraSetFlags = [
+            "--advertise-routes=192.168.1.0/24"
+            "--advertise-exit-node"
+        ];
+    };
+    networking.firewall.allowedUDPPorts = [ 41641 ];
+    networking.firewall.allowedTCPPorts = [ 443 ];
 }
