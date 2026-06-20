35c83c0a84c23d51b7a14536bc89195b6853bb58
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-20 08:01:55 +0100

    changed stuff

--- rpi/modules/nixos/tailscale.nix
+++ rpi/modules/nixos/tailscale.nix
@@ -3,10 +3,6 @@
     services.tailscale = {
         enable = true;
         useRoutingFeatures = "server";
-        extraUpFlags = [
-            "--advertise-routes=192.168.1.0/24"
-            "--advertise-exit-node"
-        ];
     };
     networking.firewall.allowedUDPPorts = [ 41641 ];
     networking.firewall.allowedTCPPorts = [ 443 ];
