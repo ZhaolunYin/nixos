4f3edd7a620fa6e7f972aefa74bcab2cbb19ded6
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-23 20:03:04 +0100

    fixed nvidia.nix

--- homelab/modules/nixos/nvidia.nix
+++ homelab/modules/nixos/nvidia.nix
@@ -1,28 +1,25 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
-    nixpkgs.config.allowUnfree = true;
-
-    hardware.graphics = {
-        enable = true;
+    nixpkgs.config = {
+        allowUnfree = true;
+        cudaSupport = true;
     };
+
+    hardware.graphics.enable = true;
 
     services.xserver.videoDrivers = [ "nvidia" ];
 
     hardware.nvidia = {
+        # Required for modern NVIDIA drivers
         modesetting.enable = true;
 
-        powerManagement.enable = false;
-        powerManagement.finegrained = false;
-
+        # RTX 5070 requires the proprietary driver
         open = false;
 
         nvidiaSettings = true;
 
+        # Use the latest stable driver packaged with your kernel
         package = config.boot.kernelPackages.nvidiaPackages.stable;
-        nixpkgs.config.cudaSupport = true;
-        # RTX 5070 is sm_120 cuda architecture
-        nixpkgs.config.cudaCapabilities = [ "sm_120" ]; 
     };
 }
-
