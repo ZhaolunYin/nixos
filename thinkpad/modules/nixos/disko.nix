a72c20bc3cb75b2d89cd4d0a128d67d69d40434e
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-23 20:36:29 +0100

    fixed disko

--- thinkpad/modules/nixos/disko.nix
+++ thinkpad/modules/nixos/disko.nix
@@ -4,6 +4,7 @@
         disk = {
             main = {
                 type = "disk";
+                device = "/dev/nvme0n1";
                 content = {
                     type = "gpt";
                     partitions = {
