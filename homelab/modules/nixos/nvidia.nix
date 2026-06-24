13d0b6f035862df803bb8e66677a90d810f1977c
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-24 21:23:42 +0100

    changed stuff

--- homelab/modules/nixos/nvidia.nix
+++ homelab/modules/nixos/nvidia.nix
@@ -26,8 +26,6 @@
 
         nvidiaSettings = true;
 
-        cudaSupport = true;
-
         # Use the latest stable driver packaged with your kernel
         package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
