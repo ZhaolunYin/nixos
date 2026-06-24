40f02f563c641209350f6bfd8140baa4751f8b1f
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-24 20:48:10 +0100

    changed stuff

--- homelab/modules/nixos/nvidia.nix
+++ homelab/modules/nixos/nvidia.nix
@@ -1,6 +1,15 @@
 { config, ... }:
 
 {
+    nix.settings = {
+        substituters = [
+            "https://cache.nixos-cuda.org"
+        ];
+        trusted-public-keys = [
+            "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+        ];
+    };
+
     nixpkgs.config = {
         allowUnfree = true;
         cudaSupport = true;
@@ -21,4 +30,6 @@
         # Use the latest stable driver packaged with your kernel
         package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
+    
+    nixpkgs.config.cudaSupport = true;
 }
