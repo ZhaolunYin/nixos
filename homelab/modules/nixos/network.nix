8a29e305d805a2ea14b258f8c2e14687c095a3d5
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-26 19:35:02 +0100

    added bridge device

--- homelab/modules/nixos/network.nix
+++ homelab/modules/nixos/network.nix
@@ -7,14 +7,13 @@
     };
 
     networking = {
-        networkmanager.enable = true;
         interfaces.eno1 = {
             ipv4.addresses = [{
                 address = "192.168.1.10";
                 prefixLength = 24;
             }];
         };
-        bridges.br0.interfaces = [ "eth0" "eth1" ];
+        bridges.br0.interfaces = [ "eno1" ];
         interfaces.br0.ipv4.addresses = [
             {
                 address = "192.168.1.11";
