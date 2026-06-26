efc9a556ff00038f8a9d1a518cb0c212ec708c83
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-26 21:37:37 +0100

    added caddy server

--- homelab/modules/nixos/services/caddy.nix
+++ homelab/modules/nixos/services/caddy.nix
@@ -1,7 +1,7 @@
 {
     services.caddy = {
         enable = true;
-        virtualHosts."192.168.1.11".extraConfig = ''
+        virtualHosts.":80".extraConfig = ''
             handle /llama/* {
                 uri strip_prefix /llama
                 reverse_proxy 127.0.0.1:8080
