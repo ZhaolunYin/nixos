07153a292ad2d73aa2a87cc344e471e19c4d6f50
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-26 18:11:08 +0100

    fixed firewall rules and added firewall module

--- homelab/modules/nixos/services/default.nix
+++ homelab/modules/nixos/services/default.nix
@@ -1,5 +1,6 @@
 {
     imports = [
+        ./firewall.nix
         ./llama.nix
     ];
 }
---
+++ homelab/modules/nixos/services/firewall.nix
@@ -0,0 +1,8 @@
+{
+    networking.firewall = {
+        allowedTCPPorts = [
+            22
+            8080
+        ];
+    };
+}
