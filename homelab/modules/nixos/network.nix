2040528a12465dd99a52a2ff9bdcfa5233a9a1d2
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-26 20:05:04 +0100

    changed network settings

--- homelab/modules/nixos/network.nix
+++ homelab/modules/nixos/network.nix
@@ -4,6 +4,12 @@
     services.avahi = {
         enable = true;
         nssmdns4 = true;
+        publish = {
+            enable = true;
+            addresses = true;
+            workstation = true;
+        };
+        openFirewall = true;
     };
 
     networking = {
