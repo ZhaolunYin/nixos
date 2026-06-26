9a213967febffb014003bd1f8b3501f6daeeab61
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-26 18:23:08 +0100

    changed ip address

--- homelab/modules/nixos/network.nix
+++ homelab/modules/nixos/network.nix
@@ -8,6 +8,13 @@
 
     networking = {
         networkmanager.enable = true;
+        interfaces.eno1 = {
+            ipv4.addresses = [{
+                address = "192.168.1.10";
+                prefixLength = 24;
+            }];
+        };
+        defaultGateway = "192.168.1.1";
         nameservers = [
             "1.1.1.1"
             "8.8.8.8"
