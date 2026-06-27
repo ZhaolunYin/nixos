e807a329459a70ddf75fd3120cc00a92b5f592eb
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-27 07:22:35 +0100

    changed firewall rules

--- homelab/modules/nixos/services/caddy.nix
+++ homelab/modules/nixos/services/caddy.nix
@@ -4,6 +4,7 @@
         virtualHosts."http://localhost" = {
             extraConfig = ''
             handle /llama* {
+                uri strip_prefix /llama
                 reverse_proxy localhost:2000
             }
             handle {
