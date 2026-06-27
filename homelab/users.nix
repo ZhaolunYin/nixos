183396bb9209e94b8822d493fca5ba69f23d8d0a
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-27 18:32:42 +0100

    added power settings

--- homelab/users.nix
+++ homelab/users.nix
@@ -11,7 +11,7 @@
     };
     users.users.yxk = {
         isNormalUser = true;
-        extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+        extraGroups = [ "networkmanager" "libvirtd" ];
         hashedPassword = "$6$Dt.CpimXXd3AOKWS$ptEUS2z4TZp8IZjbA1GYF7XBdMb3I0Kx2wugi.6o4VGenV565TrTAK.DmdcdWDnR6rVh6Htl60E.n3ITCkMgU/";
         packages = with pkgs; [
             remmina
