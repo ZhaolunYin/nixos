b9c0a54147b7ecd96aa742d491525acb2b681162
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-06-30 18:54:12 +0100

    added steam

--- homelab/users.nix
+++ homelab/users.nix
@@ -24,6 +24,7 @@
         isNormalUser = true;
         extraGroups = [ "video" "render" "networkmanager" "uinput" ];
         hashedPassword = "$6$2zvICzUP99Ww87pL$u.o6R7LRjA3m1LdWzWo2u1r6WABZgecEvNqDpk1tQTglDQTFvg9sI5JRcGuFPsKx641l58M.g91w8D7QkgUq90";
+        programs.steam.enable = true;
     };
     users.users.yxk = {
         isNormalUser = true;
