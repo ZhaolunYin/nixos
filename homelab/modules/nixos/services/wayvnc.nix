773bd6c88604983a5fb74a27f32c38066966f054
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-07-03 16:38:41 +0100

    added wayvnc

--- homelab/modules/nixos/services/wayvnc.nix
+++ homelab/modules/nixos/services/wayvnc.nix
@@ -1,9 +1,24 @@
+{ pkgs, ... }:
 {
-    services.wayvnc = {
-        enable = true;
-        settings.default = {
-            address = "0.0.0.0";
-            enable_auth = false; # set up a cert/password if exposing beyond localhost
+    services = {
+        desktopManager.plasma6.enable = false;
+        displayManager.sddm.enable = false;
+    };
+
+    programs = {
+        sway = {
+            enable = true;
+            wrapperFeatures.base = true;
+            wrapperFeatures.gtk = true;
+            xwayland.enable = true;
+        };
+
+        wayvnc = {
+            enable = true;
         };
     };
+
+    environment.etc."sway/config.d/wayvnc.conf".text = ''
+        exec ${pkgs.wayvnc}/bin/wayvnc &
+    '';
 }
