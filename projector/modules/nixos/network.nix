787056cbe8d9de7c6b4363a62258963e3a08b3d3
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-07-06 12:09:05 +0100

    added ;

--- projector/modules/nixos/network.nix
+++ projector/modules/nixos/network.nix
@@ -19,7 +19,7 @@
             ipv4.addresses = [{
                 address = "192.168.1.136";
                 prefixLength = 24;
-            }]
+            }];
         };
         defaultGateway = "192.168.1.1";
         nameservers = [
