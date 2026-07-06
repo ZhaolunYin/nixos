61e754e71178d7b86c20597b99a17b7b5e43dc57
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-07-06 12:05:29 +0100

    fixed mouse

--- projector/modules/nixos/network.nix
+++ projector/modules/nixos/network.nix
@@ -14,6 +14,14 @@
             enable = true;
             wifi.backend = "iwd";
         };
+        useDHCP = false;
+        interfaces.wlan0 = {
+            ipv4.addresses = [{
+                address = "192.168.1.136";
+                prefixLength = 24;
+            }]
+        };
+        defaultGateway = "192.168.1.1";
         nameservers = [
             "1.1.1.1"
             "8.8.8.8"
