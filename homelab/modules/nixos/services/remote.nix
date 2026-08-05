28719c4b7efd25a476099d55d1b8aae028beef89
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-08-05 18:44:03 +0100

    added homelab as remote builder for x60s

--- homelab/modules/nixos/services/remote.nix
+++ homelab/modules/nixos/services/remote.nix
@@ -2,4 +2,8 @@
     boot.binfmt.emulatedSystems = [
         "i686-linux"
     ];
+    nix.settings = {
+        trusted-users = [ "root" "lab" ];
+        extra-platforms = [ "i686-linux" ];
+    };
 }
