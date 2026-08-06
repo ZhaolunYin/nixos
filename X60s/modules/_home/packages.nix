bc90a22f29275b5bf676341937ad0d08d8edbbe1
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-08-06 12:29:36 +0100

    installed btop

--- X60s/modules/_home/packages.nix
+++ X60s/modules/_home/packages.nix
@@ -1,6 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
+let 
+    unstable = import inputs.nixpkgs {
+        inherit (pkgs.stdenv.hostPlatform) system;
+        config = pkgs.config;
+    };
+in
 {
     home.packages = with pkgs; [
         kdePackages.falkon
+        unstable.btop
     ];
 }
