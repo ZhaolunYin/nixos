360697fb5bf0e07d5a76bff81df30c271fdab982
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-08-06 13:59:14 +0100

    installed btop on homelab

--- homelab/users.nix
+++ homelab/users.nix
@@ -18,14 +18,19 @@
         isNormalUser = true;
         extraGroups = [ "wheel" "networkmanager" ];
         hashedPassword = "$y$j9T$Cur1cOb.2uQWpyl4zQrYZ/$28ebqpkeYmFAI4eHXDIkmt/gNnvIzTyYIJUbdCqIYq6";
+        packages = with pkgs; [
+            btop
+            firefox
+        ];
     };
     users.users.yxk = {
         isNormalUser = true;
         extraGroups = [ "wheel" "networkmanager" "libvirtd" ];
         hashedPassword = "$6$Dt.CpimXXd3AOKWS$ptEUS2z4TZp8IZjbA1GYF7XBdMb3I0Kx2wugi.6o4VGenV565TrTAK.DmdcdWDnR6rVh6Htl60E.n3ITCkMgU/";
         packages = with pkgs; [
-            remmina
+            btop
             firefox
+            remmina
         ];
     };
     users.users.root.hashedPassword = "!"; 
