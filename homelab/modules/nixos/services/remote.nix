9ded75128d611310d4e9843c27aa2c45fc3eb971
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-08-12 15:38:12 +0100

    added big parallel to homelab

--- homelab/modules/nixos/services/remote.nix
+++ homelab/modules/nixos/services/remote.nix
@@ -5,5 +5,6 @@
     nix.settings = {
         trusted-users = [ "root" "lab" ];
         extra-platforms = [ "i686-linux" ];
+        system-features = [ "kvm" "nixos-test" "big-parallel" ];
     };
 }
