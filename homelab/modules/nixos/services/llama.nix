84b390b874505e31e40c2651780f7eedf77eb9ca
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-08-26 08:00:40 +0100

    switched to qwen 3.8

--- homelab/modules/nixos/services/llama.nix
+++ homelab/modules/nixos/services/llama.nix
@@ -8,9 +8,11 @@
 
 
         settings = {
-            hf-repo = "unsloth/Qwen3.6-35B-A3B-GGUF";
-            hf-file = "Qwen3.6-35B-A3B-UD-Q4_K_XL.gguf";
-            alias = "unsloth/Qwen3.6-35B-A3B";
+            # hf-repo = "unsloth/Qwen3.6-35B-A3B-GGUF";
+            # hf-file = "Qwen3.6-35B-A3B-UD-Q4_K_XL.gguf";
+            hf-repo = "unsloth/Qwen3.8-27B-GGUF";
+            hf-file = "Qwen3.8-27B-UD-IQ3_XXS.gguf";
+            alias = "Qwen3.8";
 
             ctx-size = 131072;
             fit = "on";
--- thinkpad/modules/_home/pi.nix
+++ thinkpad/modules/_home/pi.nix
@@ -2,6 +2,7 @@
 {
     home.packages = with pkgs; [
         pi-coding-agent
+        nodejs
     ];
 
     home.file = {
