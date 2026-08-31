78e4239ec69f335d17921422a361bea06ebdb5dc
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-08-31 20:52:35 +0100

    changed to use model router

--- homelab/modules/nixos/services/llama.nix
+++ homelab/modules/nixos/services/llama.nix
@@ -5,27 +5,31 @@
         package = inputs.llama-cpp.packages.${pkgs.stdenv.hostPlatform.system}.cuda;
 
         settings = {
-            # hf-repo = "unsloth/Qwen3.6-35B-A3B-GGUF";
-            # hf-file = "Qwen3.6-35B-A3B-UD-Q4_K_XL.gguf";
             # hf-repo = "unsloth/Qwen3.8-27B-GGUF";
             # hf-file = "Qwen3.8-27B-UD-IQ3_XXS.gguf";
             # hf-repo = "ornith-ai/Ornith-1.5-35B-A3B-GGUF";
             # hf-file = "Ornith-1.5-35B-Q4_K_M.gguf";
-            # hf-repo = "unsloth/Qwen3.8-Flash-Next-GGUF:UD-IQ3_XXS";
-            hf-repo = "unsloth/Qwen3.8-Flash-Next-GGUF:UD-IQ1_M";
-            alias = "Qwen3.8-Flash-Next";
 
             device = "CUDA0";
             ctx-size = 0;
 
-            load-mode = "mmap";
-            tensor-read-lazy = "on";
             flash-attn = "on";
 
             host = "0.0.0.0";
             port = 2001;
             threads = 8;
             parallel = 1;
+
+            models-preset = (pkgs.formats.ini { }).generate "models-preset.ini" {
+                "Qwen3.6-35B-A3B" = {
+                    hf-repo = "unsloth/Qwen3.6-35B-A3B-MTP-GGUF:UD-Q4_K_XL";
+                };
+                "Qwen3.8-Flash-Next" = {
+                    hf-repo = "unsloth/Qwen3.8-Flash-Next-GGUF:UD-IQ1_M";
+                    load-mode = "mmap";
+                    tensor-read-lazy = "on";
+                };
+            };
         };
     };
 
