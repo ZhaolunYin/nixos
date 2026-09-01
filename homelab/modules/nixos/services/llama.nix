50c8f7cbcda6c81851c32af52449206530301898
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-09-01 21:16:58 +0100

    experiment lower context

--- homelab/modules/nixos/services/llama.nix
+++ homelab/modules/nixos/services/llama.nix
@@ -20,7 +20,7 @@
             cache-type-v = "q4_0";
 
             # ctx-size = 98304;
-            ctx-size = 8192;
+            ctx-size = 16384;
             /*models-max = 1;
             models-preset = (pkgs.formats.ini { }).generate "models-preset.ini" {
                 "Qwen3.6-35B-A3B" = {
