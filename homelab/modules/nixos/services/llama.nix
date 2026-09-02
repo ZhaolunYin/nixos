714ae4a4680346aa25ac7b3b7ff0157006514b03
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-09-02 20:45:39 +0100

    canged spec

--- homelab/modules/nixos/services/llama.nix
+++ homelab/modules/nixos/services/llama.nix
@@ -54,10 +54,7 @@
                     spec-draft-n-max = 4;
                     spec-ngram-mod-n-match = 24;
                     spec-ngram-mod-n-min = 24;
-
-                    /*
                     spec-ngram-mod-n-max = 128;
-                    */
 
                     no-mmproj = true;
                     cpu-moe = true;
