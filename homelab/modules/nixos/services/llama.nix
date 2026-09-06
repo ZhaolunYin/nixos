4cab02f1f11250761869889582f92eeeb6b8b39a
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-09-06 20:40:35 +0100

    removed fancy stuff

--- homelab/modules/nixos/services/llama.nix
+++ homelab/modules/nixos/services/llama.nix
@@ -1,10 +1,4 @@
 { pkgs, llama-cpp, ... }:
-let
-    qwen-3-8-27b-mtp = pkgs.fetchurl {
-        url = "https://huggingface.co/unsloth/Qwen3.8-27B-GGUF/resolve/main/MTP/mtp-Qwen3.8-27B-Q4_0.gguf";
-        hash = "sha256-UNnOWm2jgbvPsxBhz3PflKkOb6+O/t3uN5qcuPFQHG4=";
-    };
-in
 {
     services.llama-cpp = {
         enable = true;
@@ -58,20 +52,10 @@ in
                 "Qwen3.8-27B-Q4" = {
                     hf-repo = "unsloth/Qwen3.8-27B-GGUF:UD-Q4_K_M";
 
-
-                    spec-type = "draft-mtp,ngram-mod";
-                    spec-draft-n-max = 2;
-                    spec-draft-model = "${qwen-3-8-27b-mtp}";
-                    spec-draft-ngl = 0;
-
-                    n-cpu-ffn = 10;
-
                     cache-type-k = "q4_0";
                     cache-type-v = "q4_0";
-                    cache-type-k-draft = "q4_0";
-                    cache-type-v-draft = "q4_0";
 
-                    ctx-size = 0;
+                    ctx-size = 65536;
                 };
                 "Qwen3.8-Flash-Next" = {
                     hf-repo = "unsloth/Qwen3.8-Flash-Next-GGUF:UD-Q4_K_XL";
