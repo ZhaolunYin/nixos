4a255abaebaea1963252af862f95f2985482cfab
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-09-25 18:06:55 +0100

    removed unused caches

--- homelab/modules/nixos/nvidia.nix
+++ homelab/modules/nixos/nvidia.nix
@@ -4,11 +4,9 @@
     nix.settings = {
         extra-substituters = [
             "https://cache.nixos-cuda.org"
-            "https://cuda-maintainers.cachix.org"
         ];
         extra-trusted-public-keys = [
             "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-            "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         ];
     };
 
